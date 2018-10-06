9c9c432c79a88da13e811af3b898ef8237f3c8b6
Mateusz Sawa <mateusz.sawa@circlekeurope.com>
2018-10-06 15:57:04 +0200

    Check

--- pipeline.sh
+++ pipeline.sh
@@ -4,11 +4,9 @@ COMMAND=$1
 VERSION=$(cat version.txt)
 
 updateVersion() {
-    git checkout master
-    git pull
+    git checkout master && git pull
     echo "x=$VERSION + 0.01; if(x<1) print 0; x" | bc | tee version.txt
-    git add .
-    git commit -m version
+    git add . && git commit -m version
 }
 
 case $COMMAND in
